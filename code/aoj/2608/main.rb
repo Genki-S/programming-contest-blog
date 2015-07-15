90493c637bf7459704410228d6e587988cd37928
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2015-07-15 12:20:40 +0900

    AOJ 2608 AC

--- code/aoj/2608/main.rb
+++ code/aoj/2608/main.rb
@@ -92,107 +92,57 @@ end
 
 # }}}
 
-class Heap
+# io.set_input('inputs/4.txt')
+
+class Node
+  attr_accessor :neighbors
   def initialize
-    @arr = []
+    @neighbors = Set.new
   end
 
-  def push(val)
-    @arr << val
-    @arr.first, @arr.last = @arr.last, @arr.first
-    make_heap!
-  end
-
-  def top
-    @arr.first
-  end
-
-  def pop
-    ret = top
-    @arr.first = @arr.pop
-    make_heap!
-    ret
-  end
-
-  private
-
-  def make_heap!
-    i = 0
-    while i < @arr.size
-      i = @arr.size # make it finish by default
-      [left(i), right(i)].each do |j|
-        if j < @arr.size && @arr[j] < @arr[i]
-          @arr[i], @arr[j] = @arr[j], @arr[i]
-          i = j
-          break
-        end
-      end
-    end
-  end
-
-  def left(i)
-    i * 2 + 1
-  end
-
-  def right(i)
-    i * 2 + 2
-  end
-
-  def parent(i)
-    (i - 1) / 2
+  def connect(n)
+    @neighbors << n
   end
 end
-
-class Data
-  attr_reader :cost, :node
-
-  def initialize(cost, node)
-    @cost, @node = cost, node
-  end
-
-  def <(rhs)
-    if @cost == rhs.cost
-      @node < rhs.node
-    else
-      @cost < rhs.cost
-    end
-  end
-end
-
-io.set_input('inputs/1.txt')
 
 n, m, s, t = io.ints
 s -= 1
 t -= 1
-g = Array.new(n) { Array.new(n, false) }
+nodes = Array.new(n) { Node.new }
 m.times do
   x, y = io.ints
-  g[x-1][y-1] = g[y-1][x-1] = true
+  x -= 1
+  y -= 1
+  nodes[x].connect(y)
+  nodes[y].connect(x)
 end
 
-paths = Array.new(n, 0)
-cost = Array.new(n, INF)
-visited = Array.new(n, false)
-paths[s] = 1
-cost[s] = 1
-visited[s] = true
-nodes = Set.new([s])
-while !nodes.empty?
-  nex = Set.new
-  nodes.each do |i|
+min_dist = INF
+nodes_by_dist_from_start = Hash.new { |h, k| h[k] = Set.new }
+nodes_by_dist_from_end = Hash.new { |h, k| h[k] = Set.new }
+[s, t].each do |start|
+  visited = Array.new(n, false)
+  q = []
+  q << [start, 0]
+  while !q.empty?
+    i, d = q.shift
+    next if visited[i]
     visited[i] = true
-    n.times do |j|
-      next if i == j
-      next unless g[i][j]
-      next if visited[j]
-      cost[j] = cost[i] + 1
-      paths[j] += paths[i]
-      nex << j
+    min_dist = d if start == s && i == t
+    nodes_by_dist_from_start[d] << i if start == s
+    nodes_by_dist_from_end[d] << i if start == t
+    nodes[i].neighbors.each do |j|
+      q.push([j, d+1])
     end
   end
-  nodes = nex
 end
 
-puts [0, cost[t] - 2].max * paths[t]
+ans = 0
+(0...min_dist).each do |i|
+  j = min_dist - i - 2
+  ans += nodes_by_dist_from_start[i].size * nodes_by_dist_from_end[j].size
+end
+
+puts ans
 
 # vim: foldmethod=marker
